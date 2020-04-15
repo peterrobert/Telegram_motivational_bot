f75ffda4d7ae438c9db04ba00a353fe39bb10cfd
peterrobert <pwambui93@gmail.com>
2020-04-15 13:15:01 +0300

    fixed rubocop offences for motivate.rb file

--- lib/motivate.rb
+++ lib/motivate.rb
@@ -1,31 +1,28 @@
+# rubocop:disable Style/ClassVars
 require 'telegram/bot'
 require 'net/http'
 require 'json'
 require_relative 'bot.rb'
 
 class Motivate
+  @@values = nil
 
-   @@values = nil
+  def initialize
+    @@values = make_the_request
+  end
 
-  def initialize 
-    @@values = make_the_request   
-  end 
-
-  
   def make_the_request
     url = 'https://type.fit/api/quotes'
     uri = URI(url)
     response = Net::HTTP.get(uri)
     response = JSON.parse(response)
     response
-  
   end
 
   def select_random
     @@values = @@values.sample
     @@values
   end
-
-   
 end
 
+# rubocop: enable Style/ClassVars
