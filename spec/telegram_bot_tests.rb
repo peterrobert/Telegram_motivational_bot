ab07fc3a92ef890a08820e7f7c7fe5a3e110da72
peterrobert <pwambui93@gmail.com>
2020-04-15 22:05:08 +0300

    updated the tests and made them more dry

--- spec/telegram_bot_tests.rb
+++ spec/telegram_bot_tests.rb
@@ -3,47 +3,41 @@ require_relative '../lib/joke.rb'
 
 RSpec.describe Motivate do
   describe '#make_the_request' do
-  it 'should return json response when the request is sucessful' do
-    request = Motivate.new
-    request_json = request.make_the_request
+    let(:request) { Motivate.new }
+    let(:request_json) { request.make_the_request }
 
-    expect(request_json.class).to eql(Array)
-  end
+    it 'should return json response when the request is sucessful' do
+      expect(request_json.class).to eql(Array)
+    end
 
-  it 'returns an array response should not be empty' do
-    request = Motivate.new
-    request_json = request.make_the_request
-    expect(request_json.length).not_to eql(0)
-  end
+    it 'returns an array response should not be empty' do
+      expect(request_json.length).not_to eql(0)
+    end
   end
 
   describe '#select_random' do
-  it 'should return an Hash' do
-    random = Motivate.new
-    request_random = random.select_random
+    let(:random) { Motivate.new }
+    let(:request_random) { random.select_random }
 
-    expect(request_random.class).to eql(Hash)
-  end
+    it 'should return an Hash' do
+      expect(request_random.class).to eql(Hash)
+    end
 
-  it 'should return a key and a value' do
-    random = Motivate.new
-    request_random = random.select_random
-    expect(request_random.length).to eql(2)
-  end
+    it 'should return a key and a value' do
+      expect(request_random.length).to eql(2)
+    end
   end
 end
 
 RSpec.describe Joke do
   describe '#make_the_request' do
-  it 'should return json response when the request is sucessful' do
-    request = Motivate.new
-    request_json = request.make_the_request
-    expect(request_json.class).to eql(Array)
-  end
-  it 'returns an array response should not be empty' do
-    request = Motivate.new
-    request_json = request.make_the_request
-    expect(request_json.length).not_to eql(0)
-  end
+    let(:request) { Joke.new }
+    let(:request_json) { request.make_the_request }
+    it 'should return hash response when the request is sucessful' do
+      expect(request_json.class).to eql(Hash)
+    end
+    it 'returns an array response should not be empty' do
+      expect(request_json.length).not_to eql(0)
+    end
   end
 end
