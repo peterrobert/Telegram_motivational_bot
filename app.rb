d6a510d1fed5c1f0d97e76f5a3f278537cc649d3
peterrobert <pwambui93@gmail.com>
2020-04-15 13:19:30 +0300

    updated the app.rb file

--- app.rb
+++ app.rb
@@ -7,5 +7,6 @@ puts 'This is a bot to help you stay motivated by giving you motivational quotes
 puts 'The bot can also keep you entertained by providing you with jokes'
 puts 'This bolt will help you when you feeling down and demotivated'
 sleep(0.6)
+puts '----------------------------------------------'
 puts '...loading the bot is now online... use ctrl-c to stop the bot'
 Bot.new
