214ce0e0ec87f271e2f7feadc8d57beefa0af1f6
peterrobert <pwambui93@gmail.com>
2020-04-15 15:30:38 +0300

    fixed bots.rb offences

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -21,10 +21,10 @@ RSpec.configure do |config|
    # This option will default to `true` in RSpec 4. It makes the `description`
    # and `failure_message` of custom matchers include text for helper methods
    # defined using `chain`, e.g.:
-   #    be_bigger_than(2).and_smaller_than(4).description
-   #    # => "be bigger than 2 and smaller than 4"
+   #  be_bigger_than(2).and_smaller_than(4).description
+   #  # => "be bigger than 2 and smaller than 4"
    # ...rather than:
-   #    # => "be bigger than 2"
+   #  # => "be bigger than 2"
    expectations.include_chain_clauses_in_custom_matcher_descriptions = true
   end
 
@@ -88,7 +88,7 @@ RSpec.configure do |config|
   # Run specs in random order to surface order dependencies. If you find an
   # order dependency and want to debug it, you can fix the order by providing
   # the seed, which is printed after each run.
-  #    --seed 1234
+  #  --seed 1234
   config.order = :random
 
   # Seed global randomization in this process using the `--seed` CLI option.
--- spec/telegram_bot_tests.rb
+++ spec/telegram_bot_tests.rb
@@ -3,47 +3,47 @@ require_relative '../lib/joke.rb'
 
 RSpec.describe Motivate do
   describe '#make_the_request' do
-    it 'should return json response when the request is sucessful' do
-      request = Motivate.new
-      request_json = request.make_the_request
+  it 'should return json response when the request is sucessful' do
+    request = Motivate.new
+    request_json = request.make_the_request
 
-      expect(request_json.class).to eql(Array)
-    end
+    expect(request_json.class).to eql(Array)
+  end
 
-    it 'returns an array response should not be empty' do
-      request = Motivate.new
-      request_json = request.make_the_request
-      expect(request_json.length).not_to eql(0)
-    end
+  it 'returns an array response should not be empty' do
+    request = Motivate.new
+    request_json = request.make_the_request
+    expect(request_json.length).not_to eql(0)
+  end
   end
 
   describe '#select_random' do
-    it 'should return an Hash' do
-      random = Motivate.new
-      request_random = random.select_random
+  it 'should return an Hash' do
+    random = Motivate.new
+    request_random = random.select_random
 
-      expect(request_random.class).to eql(Hash)
-    end
+    expect(request_random.class).to eql(Hash)
+  end
 
-    it 'should return a key and a value' do
-      random = Motivate.new
-      request_random = random.select_random
-      expect(request_random.length).to eql(2)
-    end
+  it 'should return a key and a value' do
+    random = Motivate.new
+    request_random = random.select_random
+    expect(request_random.length).to eql(2)
+  end
   end
 end
 
 RSpec.describe Joke do
   describe '#make_the_request' do
-    it 'should return json response when the request is sucessful' do
-      request = Motivate.new
-      request_json = request.make_the_request
-      expect(request_json.class).to eql(Array)
-    end
-    it 'returns an array response should not be empty' do
-      request = Motivate.new
-      request_json = request.make_the_request
-      expect(request_json.length).not_to eql(0)
-    end
+  it 'should return json response when the request is sucessful' do
+    request = Motivate.new
+    request_json = request.make_the_request
+    expect(request_json.class).to eql(Array)
+  end
+  it 'returns an array response should not be empty' do
+    request = Motivate.new
+    request_json = request.make_the_request
+    expect(request_json.length).not_to eql(0)
+  end
   end
 end
